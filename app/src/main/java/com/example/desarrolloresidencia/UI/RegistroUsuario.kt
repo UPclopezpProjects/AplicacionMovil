f6cb3de2e36fb018c8c634f04be79dda152fb91e
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2021-03-04 13:20:50 -0600

    ando haciendo el registro de usuarios

--- app/src/main/java/com/example/desarrolloresidencia/UI/RegistroUsuario.kt
+++ app/src/main/java/com/example/desarrolloresidencia/UI/RegistroUsuario.kt
@@ -1,12 +1,63 @@
 package com.example.desarrolloresidencia.UI
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.desarrolloresidencia.R
 
 class RegistroUsuario : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
+
+        var registro = findViewById<Button>(R.id.BTRegistrar)
+        registro.setOnClickListener {
+            ValidationE()
+        }
+    }
+
+    fun ValidationE(){
+        var nombre = findViewById<EditText>(R.id.ETNombre)
+        var apellidoP = findViewById<EditText>(R.id.ETApellidoP)
+        var apellidoM = findViewById<EditText>(R.id.ETApellidoM)
+        var username = findViewById<EditText>(R.id.ETUsername)
+        var contrasena = findViewById<EditText>(R.id.ETContrasena)
+
+        if (nombre.text.toString() ==""){
+            nombre.error = "Ingresa el nombre"
+            nombre.requestFocus()
+            return
+        }
+
+        if (apellidoP.text.toString() ==""){
+            apellidoP.error = "Ingresa el apellido paterno"
+            apellidoP.requestFocus()
+            return
+        }
+
+        if (apellidoM.text.toString() ==""){
+            apellidoM.error = "Ingresa el apellido materno"
+            apellidoM.requestFocus()
+            return
+        }
+
+        if (username.text.toString() ==""){
+            username.error = "Ingresa el Email"
+            username.requestFocus()
+            return
+        }
+
+        if (contrasena.text.toString() ==""){
+            contrasena.error = "Ingresa la contraseña"
+            contrasena.requestFocus()
+            return
+        }
+
+        var intent: Intent = Intent(applicationContext, Login::class.java)
+        startActivity(intent)
+
     }
 }
---
+++ app/src/main/java/com/example/desarrolloresidencia/ViewModel/RegistroViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.desarrolloresidencia.ViewModel
+
+class RegistroViewModel {
+}
