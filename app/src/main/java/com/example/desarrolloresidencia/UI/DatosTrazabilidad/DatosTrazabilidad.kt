ddd5e7be310ecb1448a7a102042325905dfdb07d
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2021-03-17 17:06:03 -0600

    recycler view y mapa
    ya salen ambos

--- app/src/main/java/com/example/desarrolloresidencia/UI/DatosTrazabilidad/DatosTrazabilidad.kt
+++ app/src/main/java/com/example/desarrolloresidencia/UI/DatosTrazabilidad/DatosTrazabilidad.kt
@@ -1,12 +1,43 @@
 package com.example.desarrolloresidencia.UI.DatosTrazabilidad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.desarrolloresidencia.R
 
-class DatosTrazabilidad : AppCompatActivity() {
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class DatosTrazabilidad : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_fragmento)
+        setContentView(R.layout.datos_trazabilidad)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        var back = findViewById<Button>(R.id.BTVolver)
+        back.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
